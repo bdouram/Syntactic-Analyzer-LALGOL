87bf6b432d2440368d0fbc6afe4622a095e2a9d1
Bruno Dourado Miranda <31232196+bdouram@users.noreply.github.com>
2017-08-22 00:36:01 -0400

    Create Objeto.java

---
+++ Objeto.java
@@ -0,0 +1,13 @@
+
+package compiladorlalgol;
+
+/**
+ *
+ * @author Bruno Dourado Miranda
+ */
+public class Objeto {
+    String token;
+    String identificador;
+    int numeroLinha;
+    Objeto prox;
+}
